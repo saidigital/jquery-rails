0fc3396fac0b562d8a14a6c4f228cd234f8d71c7
Sean Brewer <seabre986@gmail.com>
2012-05-31 16:24:26 -0400

    Removed JQuery UI.

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -22,14 +22,6 @@ if ::Rails.version < "3.1" || !::Rails.application.config.assets.enabled
           copy_file "jquery.min.js", "public/javascripts/jquery.min.js"
         end
 
-        def copy_jquery_ui
-          if options.ui?
-            say_status("copying", "jQuery UI (#{Jquery::Rails::JQUERY_UI_VERSION})", :green)
-            copy_file "jquery-ui.js", "public/javascripts/jquery-ui.js"
-            copy_file "jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
-          end
-        end
-
         def copy_ujs_driver
           say_status("copying", "jQuery UJS adapter (#{Jquery::Rails::JQUERY_UJS_VERSION[0..5]})", :green)
           remove_file "public/javascripts/rails.js"
